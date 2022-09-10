1e19797a45cda061d9cad682c2d09f901a1412e3
Mariana Ramírez <104577792+rammariana@users.noreply.github.com>
2022-09-09 21:39:33 -0500

    Cashregister

---
+++ index.js
@@ -0,0 +1,7 @@
+import calculator from "./calculator.js";
+import totalCash from "./totalCash.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    calculator();
+    totalCash();
+});
