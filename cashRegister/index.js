3c017d626ccf2a66fb57f585d54e4076b41f681a
Mariana Ramírez <104577792+rammariana@users.noreply.github.com>
2023-01-24 22:38:35 -0500

    Delete cashRegister directory

--- cashRegister/index.js
+++
@@ -1,7 +0,0 @@
-import calculator from "./calculator.js";
-import totalCash from "./totalCash.js";
-
-document.addEventListener("DOMContentLoaded", (e) => {
-    calculator();
-    totalCash();
-});
